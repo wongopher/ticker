388b9a40698f23ecf993a7af16a3df15f487a7a9
Chris W <chris@slyce.it>
2015-05-02 14:18:12 -0400

    added basic styling and "word processor-esque" highlight effect (will need to make dynamic)

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ function Ticker(settings) {
 	this.freeze = settings.freeze;
 	this.speed = settings.speed;
 	this.words = settings.words;
+	this.settings = settings;
 	this.elem = document.getElementById(settings.elementID);
 
-	if (settings.underline == true) this.elem.style.borderBottom = "1px solid #000";
+	if (settings.underline == true) this.elem.style.borderBottom = '2px solid #000';
 
 	return this;
 }
@@ -19,16 +20,30 @@ Ticker.prototype = {
 		if (currentWord.length > i) { // print chars for length of word
 			_self.elem.innerHTML += currentWord[i];
 			++i;
+
 			setTimeout(function(){
 				_self.scribe(currentWord, i, _self.speed);
 			}, typeSpeed);
+
 		} else { // reset at end of length
+			_self.addedDelay = 0;
+			if (_self.settings.highlight == true) {
+				_self.addedDelay = 1000;
+				setTimeout(function(){
+					_self.elem.style.backgroundColor = '#000';
+					_self.elem.style.color = '#fff';
+				}, _self.freeze);
+			}
+
 			setTimeout(function(){
 				_self.elem.innerHTML = '';
 				setTimeout(function(){
+					_self.elem.style.backgroundColor = 'transparent';
+					_self.elem.style.color = '#777';
 					_self.start();
 				}, _self.freeze);
-			}, _self.freeze);
+			}, _self.freeze + _self.addedDelay);
+
 		}
 	},
 
