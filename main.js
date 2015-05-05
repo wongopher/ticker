7621c5814d26fc3daf4e95c3df7b74f8cf66a96c
Chris W <christopherphwong@gmail.com>
2015-05-05 14:23:08 -0400

    Made Ticker IIFE to hide _highlight function for higher order

--- main.js
+++ main.js
@@ -1,66 +1,74 @@
 // constructor function
-function Ticker(settings) {
-  // establish default settings
-  this.freeze     = settings.freeze || 1500;
-  this.speed      = settings.speed || Math.ceil(Math.random() * 50) + 50;
-  this.words      = settings.words || ['You need to pass a word array into your Ticker instance'];
-  this.settings   = settings;
-  this.elem       = document.getElementById(settings.elementID) || document.getElementById('ticker');
-  this.prevWord   = '';
+(function(window){
+  window.Ticker = function(settings) {
+    // establish default settings
+    this.freeze     = settings.freeze || 1500;
+    this.speed      = settings.speed || Math.ceil(Math.random() * 50) + 50;
+    this.words      = settings.words || ['You need to pass a word array into your Ticker instance'];
+    this.settings   = settings;
+    this.elem       = document.getElementById(settings.elementID) || document.getElementById('ticker');
+    this.prevWord   = '';
 
-  // set up styling -- need to move this out of the library code!!!
-  if (settings.underline == true) this.elem.style.borderBottom = '2px solid #777';
+    // set up styling -- need to move this out of the library code!!!
+    if (settings.underline == true) this.elem.style.borderBottom = '2px solid #777';
 
-  return this;
-}
-
-Ticker.prototype = {
-
-  // prints individual characters
-  scribe: function (currentWord, i, speed) {
-    var self = this;
-    var typeSpeed = speed;
-
-    if (currentWord.length > i) { // print chars for length of word
-      self.elem.innerHTML += currentWord[i];
-      ++i;
-
-      setTimeout(function(){
-        self.scribe(currentWord, i, self.speed);
-      }, typeSpeed);
-
-    } else { // reset at end of length
-      self.addedDelay = 0;
-      if (self.settings.highlight == true) {
-        self.addedDelay = 1000;
-        setTimeout(function(){
-          self.elem.style.backgroundColor = '#777';
-          self.elem.style.color = '#fff';
-        }, self.freeze);
-      }
-
-      setTimeout(function(){
-        self.elem.innerHTML = '';
-        setTimeout(function(){
-          self.elem.style.backgroundColor = 'transparent';
-          self.elem.style.color = '#777';
-          self.start();
-        }, self.freeze);
-      }, self.freeze + self.addedDelay);
-
-    }
-  },
-
-  // initiates a new word
-  start: function () {
-    var randNum = Math.floor(Math.random() * this.words.length);
-    var currentWord = this.words[randNum];
-
-    //check if new word is same as previous word
-    if (currentWord == this.prevWord) currentWord = this.words[randNum + 1] || this.words[randNum - 1];
-    this.prevWord = currentWord;
-    var i = 0;
-    this.scribe(currentWord, i, this.speed);
+    return this;
   }
 
-}
+  Ticker.prototype = {
+
+    // prints individual characters
+    scribe: function (currentWord, i, speed) {
+      var self = this;
+      var typeSpeed = speed;
+
+      if (currentWord.length > i) { // print chars for length of word
+        self.elem.innerHTML += currentWord[i];
+        ++i;
+
+        setTimeout(function(){
+          self.scribe(currentWord, i, self.speed);
+        }, typeSpeed);
+
+      } else { // reset at end of length
+        self.addedDelay = 0;
+        if (self.settings.highlight == true) {
+          self.addedDelay = 1000;
+          setTimeout(_highlight(false, self), self.freeze);
+        }
+
+        setTimeout(function(){
+          self.elem.innerHTML = '';
+          setTimeout(_highlight(true, self), self.freeze);
+        }, self.freeze + self.addedDelay);
+
+      }
+    },
+
+    // initiates a new word
+    start: function () {
+      var randNum = Math.floor(Math.random() * this.words.length);
+      var currentWord = this.words[randNum];
+
+      //check if new word is same as previous word
+      if (currentWord == this.prevWord) currentWord = this.words[randNum + 1] || this.words[randNum - 1];
+      this.prevWord = currentWord;
+      var i = 0;
+      this.scribe(currentWord, i, this.speed);
+    }
+
+  }
+
+  function _highlight(dehighlight, self) {
+    return function () {
+      if (dehighlight) {
+        self.elem.style.backgroundColor = 'transparent';
+        self.elem.style.color = '#777';
+        self.start();
+      } else {
+        self.elem.style.backgroundColor = '#777';
+        self.elem.style.color = '#fff';
+      }
+    }
+  }
+})(window);
