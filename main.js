ef30fb99a1667ebbf060cc8506b8a86e9889d39c
Chris W <christopherphwong@gmail.com>
2015-05-05 13:46:47 -0400

    Fixed indentation TODO: need to remove styling out of code

--- main.js
+++ main.js
@@ -1,65 +1,66 @@
+// constructor function
 function Ticker(settings) {
-	// establish default settings
-	this.freeze 	= settings.freeze || 1500;
-	this.speed 		= settings.speed || Math.ceil(Math.random() * 50) + 50;
-	this.words 		= settings.words || ['You need to pass a word array into your Ticker instance'];
-	this.settings 	= settings;
-	this.elem 		= document.getElementById(settings.elementID) || document.getElementById('ticker');
-	this.prevWord 	= '';
+  // establish default settings
+  this.freeze     = settings.freeze || 1500;
+  this.speed      = settings.speed || Math.ceil(Math.random() * 50) + 50;
+  this.words      = settings.words || ['You need to pass a word array into your Ticker instance'];
+  this.settings   = settings;
+  this.elem       = document.getElementById(settings.elementID) || document.getElementById('ticker');
+  this.prevWord   = '';
 
-	// set up styling 
-	if (settings.underline == true) this.elem.style.borderBottom = '2px solid #777';
+  // set up styling -- need to move this out of the library code!!!
+  if (settings.underline == true) this.elem.style.borderBottom = '2px solid #777';
 
-	return this;
+  return this;
 }
 
 Ticker.prototype = {
 
-	// prints individual characters
-	scribe: function (currentWord, i, speed) {
-		var _self = this;
-		var typeSpeed = speed;
+  // prints individual characters
+  scribe: function (currentWord, i, speed) {
+    var self = this;
+    var typeSpeed = speed;
 
-		if (currentWord.length > i) { // print chars for length of word
-			_self.elem.innerHTML += currentWord[i];
-			++i;
+    if (currentWord.length > i) { // print chars for length of word
+      self.elem.innerHTML += currentWord[i];
+      ++i;
 
-			setTimeout(function(){
-				_self.scribe(currentWord, i, _self.speed);
-			}, typeSpeed);
+      setTimeout(function(){
+        self.scribe(currentWord, i, self.speed);
+      }, typeSpeed);
 
-		} else { // reset at end of length
-			_self.addedDelay = 0;
-			if (_self.settings.highlight == true) {
-				_self.addedDelay = 1000;
-				setTimeout(function(){
-					_self.elem.style.backgroundColor = '#777';
-					_self.elem.style.color = '#fff';
-				}, _self.freeze);
-			}
+    } else { // reset at end of length
+      self.addedDelay = 0;
+      if (self.settings.highlight == true) {
+        self.addedDelay = 1000;
+        setTimeout(function(){
+          self.elem.style.backgroundColor = '#777';
+          self.elem.style.color = '#fff';
+        }, self.freeze);
+      }
 
-			setTimeout(function(){
-				_self.elem.innerHTML = '';
-				setTimeout(function(){
-					_self.elem.style.backgroundColor = 'transparent';
-					_self.elem.style.color = '#777';
-					_self.start();
-				}, _self.freeze);
-			}, _self.freeze + _self.addedDelay);
+      setTimeout(function(){
+        self.elem.innerHTML = '';
+        setTimeout(function(){
+          self.elem.style.backgroundColor = 'transparent';
+          self.elem.style.color = '#777';
+          self.start();
+        }, self.freeze);
+      }, self.freeze + self.addedDelay);
 
-		}
-	},
+    }
+  },
 
-	// initiates a new word
-	start: function () {
-		var randNum = Math.floor(Math.random() * this.words.length);
-		var currentWord = this.words[randNum];
+  // initiates a new word
+  start: function () {
+    var randNum = Math.floor(Math.random() * this.words.length);
+    var currentWord = this.words[randNum];
 
-		//check if new word is same as previous word
-		if (currentWord == this.prevWord) currentWord = this.words[randNum + 1] || this.words[randNum - 1];
-		this.prevWord = currentWord;
-		var i = 0;
-		this.scribe(currentWord, i, this.speed);
-	}
+    //check if new word is same as previous word
+    if (currentWord == this.prevWord) currentWord = this.words[randNum + 1] || this.words[randNum - 1];
+    this.prevWord = currentWord;
+    var i = 0;
+    this.scribe(currentWord, i, this.speed);
+  }
 
 }
